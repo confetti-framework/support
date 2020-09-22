a413e8055d9867b624fe0c47b2bc05041510726c
Reindert Vetter <reindert@myparcel.nl>
2020-09-22 22:37:01 +0200

    lanvard/lanvard#56 get content of file from request

--- file.go
+++ file.go
@@ -1,6 +1,38 @@
 package support
 
+import (
+	"bytes"
+	"mime/multipart"
+)
+
 type Files []File
 
 type File struct {
+	source multipart.File
+	header *multipart.FileHeader
+}
+
+func NewFile(source multipart.File, header *multipart.FileHeader) File {
+	return File{source: source, header: header}
+}
+
+func (f File) Source() multipart.File {
+	return f.source
+}
+
+func (f File) Header() *multipart.FileHeader {
+	return f.header
+}
+
+func (f File) Close() error {
+	return f.source.Close()
+}
+
+func (f File) Content() string {
+	var buff bytes.Buffer
+	_, err := buff.ReadFrom(f.source)
+	if err != nil {
+		panic(err)
+	}
+	return buff.String()
 }
