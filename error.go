e926901c978a851453363d729d2de4e2eae9cdb6
Reindert Vetter <reindert@myparcel.nl>
2020-10-21 23:30:59 +0200

    lanvard/lanvard#73 Wrap errors

--- error.go
+++ error.go
@@ -14,8 +14,19 @@ func NewError(message string) *Error {
 	return &Error{err: errors.New(message)}
 }
 
+func Wrap(err error, message string) *Error {
+	return &Error{err: errors.Wrap(err, message)}
+}
+
 func (e *Error) Error() string {
 	return e.err.Error()
+}
+
+func (e *Error) StackTrace() errors.StackTrace {
+	if errWithStack, ok := e.err.(interface{ StackTrace() errors.StackTrace }); ok {
+		return errWithStack.StackTrace()
+	}
+	return nil
 }
 
 func (e *Error) Level(inputLevel level.Level) *Error {
