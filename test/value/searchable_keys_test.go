e51369698a443a5e804f5b61644f60e88dc9233b
Reindert Vetter <reindert@myparcel.nl>
2020-11-28 17:22:22 +0100

    lanvard/lanvard#88 validate nested values

--- test/value/searchable_keys_test.go
+++ test/value/searchable_keys_test.go
@@ -9,38 +9,43 @@ import (
 
 func Test_keys_empty(t *testing.T) {
 	var given []string
-	result := support.GetSearchableKeys(given, support.NewValue(nil))
-	require.Equal(t, []string{}, result)
+	verboseKeys, settableKeys := support.GetSearchableKeys(given, support.NewValue(nil))
+	require.Equal(t, []string{}, verboseKeys)
+	require.Equal(t, []string{}, settableKeys)
 }
 
 func Test_keys_given_with_empty_value(t *testing.T) {
 	given := []string{"name", "color"}
-	result := support.GetSearchableKeys(given, support.NewValue(nil))
-	require.Equal(t, []string{"name", "color"}, result)
+	verboseKeys, settableKeys := support.GetSearchableKeys(given, support.NewValue(nil))
+	require.Equal(t, []string{"name", "color"}, verboseKeys)
+	require.Equal(t, []string{"name", "color"}, settableKeys)
 }
 
 func Test_keys_given_with_map_one_value(t *testing.T) {
 	given := []string{"name"}
 	expect := []string{"name"}
 	value := support.NewValue(support.NewMap(map[string]string{"name": "whale"}))
-	result := support.GetSearchableKeys(given, value)
-	require.Equal(t, expect, result)
+	verboseKeys, settableKeys := support.GetSearchableKeys(given, value)
+	require.Equal(t, expect, verboseKeys)
+	require.Equal(t, expect, settableKeys)
 }
 
 func Test_keys_given_with_map_two_values(t *testing.T) {
 	given := []string{"name", "color"}
 	expect := []string{"name", "color"}
 	value := support.NewValue(support.NewMap(map[string]string{"name": "mule", "color": "black"}))
-	result := support.GetSearchableKeys(given, value)
-	require.Equal(t, expect, result)
+	verboseKeys, settableKeys := support.GetSearchableKeys(given, value)
+	require.Equal(t, expect, verboseKeys)
+	require.Equal(t, expect, settableKeys)
 }
 
 func Test_keys_asterisk_given_with_map(t *testing.T) {
 	given := []string{"*"}
 	expect := []string{"name", "color"}
 	value := support.NewValue(support.NewMap(map[string]string{"name": "mule", "color": "black"}))
-	result := support.GetSearchableKeys(given, value)
-	equalStrings(t, expect, result)
+	verboseKeys, settableKeys := support.GetSearchableKeys(given, value)
+	equalStrings(t, expect, verboseKeys)
+	equalStrings(t, expect, settableKeys)
 }
 
 func Test_keys_map_with_second_map(t *testing.T) {
@@ -49,8 +54,9 @@ func Test_keys_map_with_second_map(t *testing.T) {
 	input := map[string]map[string]string{"big": {"name": "whale"}, "small": {"name": "crab"}}
 
 	value := support.NewValue(support.NewMap(input))
-	result := support.GetSearchableKeys(given, value)
-	equalStrings(t, expect, result)
+	verboseKeys, settableKeys := support.GetSearchableKeys(given, value)
+	equalStrings(t, expect, verboseKeys)
+	equalStrings(t, expect, settableKeys)
 }
 
 func Test_keys_map_with_2_layers(t *testing.T) {
@@ -59,32 +65,34 @@ func Test_keys_map_with_2_layers(t *testing.T) {
 	input := map[string]map[string]map[string]string{"animal": {"big": {"name": "whale"}}}
 
 	value := support.NewValue(support.NewMap(input))
-	result := support.GetSearchableKeys(given, value)
-	equalStrings(t, expect, result)
+	verboseKeys, settableKeys := support.GetSearchableKeys(given, value)
+	equalStrings(t, expect, verboseKeys)
+	equalStrings(t, expect, settableKeys)
 }
 
 func Test_no_keys_with_map(t *testing.T) {
 	given := []string{}
 	expect := []string{}
 	value := support.NewValue(support.NewMap(map[string]string{"name": "mule", "color": "black"}))
-	result := support.GetSearchableKeys(given, value)
-	require.Equal(t, expect, result)
+	verboseKeys, settableKeys := support.GetSearchableKeys(given, value)
+	require.Equal(t, expect, verboseKeys)
+	require.Equal(t, expect, settableKeys)
 }
 
 func Test_keys_with_collection(t *testing.T) {
 	given := []string{"*"}
-	expect := []string{"0", "1"}
 	value := support.NewValue(support.NewCollection([]string{"mule", "black"}))
-	result := support.GetSearchableKeys(given, value)
-	require.Equal(t, expect, result)
+	verboseKeys, settableKeys := support.GetSearchableKeys(given, value)
+	require.Equal(t, []string{"0", "1"}, verboseKeys)
+	require.Equal(t, []string{"*", "*"}, settableKeys)
 }
 
 func Test_keys_with_collection_and_map(t *testing.T) {
 	given := []string{"*.*"}
-	expect := []string{"0.big", "0.small"}
 	value := support.NewValue(support.NewCollection(support.NewMap(map[string]string{"big": "mule", "small": "black"})))
-	result := support.GetSearchableKeys(given, value)
-	equalStrings(t, expect, result)
+	verboseKeys, settableKeys := support.GetSearchableKeys(given, value)
+	equalStrings(t, []string{"0.big", "0.small"}, verboseKeys)
+	equalStrings(t, []string{"*.big", "*.small"}, settableKeys)
 }
 
 func equalStrings(t *testing.T, expect []string, result []string) {
