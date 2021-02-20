174dd408c5c09c99e1b5dca2ceaca0866681688a
wulfheart <dev@alexfwulf.de>
2021-02-20 11:28:09 +0100

    Outline

--- str/str.go
+++ str/str.go
@@ -19,3 +19,173 @@ func InSlice(input interface{}, expects ...interface{}) bool {
 	}
 	return false
 }
+
+func After(subject string, search string) string {
+	// TODO
+	return ""
+}
+
+func AfterLast(subject string, search string) string {
+	// TODO
+	return ""
+}
+
+func Before(subject string, search string) string {
+	// TODO
+	return ""
+}
+
+func BeforeLast(subject string, search string) string {
+	// TODO
+	return ""
+}
+
+func Between(subject string, from string, to string) string {
+	// TODO
+	return ""
+}
+
+func Contains(haystack string, needle string) bool {
+	// TODO
+	return false
+}
+
+func ContainsInSlice(haystack string, needle []string) bool {
+	// TODO
+	return false
+}
+
+func ContainsAllInSlice(haystack string, needle []string) bool {
+	// TODO
+	return false
+}
+
+func EndsWith(haystack string, needle string) bool {
+	// TODO
+	return false
+}
+
+func Finish(value string, cap string) string {
+	// TODO
+	return ""
+}
+
+func Kebab(vale string) string {
+	// TODO
+	return ""
+}
+
+func Length(value string) int {
+	// TODO
+	return 0
+}
+
+func LimitCharacters(value string, limit int, end string) string{
+	// TODO
+	return ""
+}
+
+func LimitWords(value string, limit int, end string) string{
+	// TODO
+	return ""
+}
+
+func Lower(value string) string {
+	// TODO
+	return ""
+}
+
+func PadBoth(value string, length int, pad string) string {
+	// TODO
+	return ""
+}
+
+func PadLeft(value string, length int, pad string) string {
+	// TODO
+	return ""
+}
+
+func PadRight(value string, length int, pad string) string {
+	// TODO
+	return ""
+}
+
+func ReplaceArray(search string, replace []string, subject string) string {
+	// TODO
+	return ""
+}
+
+func ReplaceFirst(search string, replace string, subject string) string {
+	// TODO
+	return ""
+}
+
+func ReplaceLast(search string, replace string, subject string) string {
+	// TODO
+	return ""
+}
+
+func Start(value string, prefix string) string {
+	// TODO
+	return ""
+}
+
+func Slug(value string) string {
+	// TODO
+	return ""
+}
+func SlugWithDelimiter(value string, delimiter string) string {
+	// TODO
+	return ""
+}
+
+func Snake(value string) string {
+	// TODO
+	return ""
+}
+
+func SnakeWithDelimiter(value string, delimiter string) string {
+	// TODO
+	return ""
+}
+
+func StartsWith(haystack string, needle string) string {
+	// TODO
+	return ""
+}
+
+func Studly(value string) string {
+	// TODO
+	return ""
+}
+
+func Upper(value string) string {
+	// TODO
+	return ""
+}
+
+func Title(value string) string {
+	// TODO
+	return ""
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
