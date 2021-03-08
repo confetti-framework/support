9e15c8588be8a0f2b22e4662d723db9413aa07e4
Reindert Vetter <reindert@myparcel.nl>
2021-03-08 22:17:56 +0100

    confetti-framework/support#9 improve test coverage

--- test/value/other_test.go
+++ test/value/other_test.go
@@ -17,9 +17,18 @@ func Test_new_invalid_value(t *testing.T) {
 }
 
 func Test_get_collection_from_value_with_asterisks(t *testing.T) {
-	require.Equal(t, []interface{}{"the_value"}, support.NewValue(support.NewCollection([]string{"the_value"})).Get("*").Raw())
+	value := support.NewValue(support.NewCollection([]string{"the_value"}))
+	require.Equal(t, []interface{}{"the_value"}, value.Get("*").Raw())
 }
 
 func Test_get_map_from_value_with_asterisks(t *testing.T) {
-	require.Equal(t, map[string]interface{}{"key": "the_value"}, support.NewValue(support.NewMap(map[string]interface{}{"key": "the_value"})).Get("*").Raw())
+	value := support.NewValue(support.NewMap(map[string]interface{}{"key": "the_value"}))
+	require.Equal(t, map[string]interface{}{"key": "the_value"}, value.Get("*").Raw())
+}
+
+func Test_get_from_invalid_value_with_asterisks(t *testing.T) {
+	value := support.NewValue("non collection/value")
+	require.Panics(t, func() {
+		value.Get("*").Raw()
+	})
 }
